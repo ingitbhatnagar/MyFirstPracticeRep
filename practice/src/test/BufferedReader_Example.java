b705bb05ae8eebfc825125ef710a272d505be1bc
ingitbhatnagar <ingitbhatnagar@gmail.com>
2017-11-09 20:08:22 +0530

    modifed test class

--- practice/src/test/BufferedReader_Example.java
+++ practice/src/test/BufferedReader_Example.java
@@ -1,36 +1,37 @@
 package test;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
 
 class BufferedReader_Example {
     public static void main(String args[] ) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        int N = Integer.parseInt(line);
-        for(int i=0;i<N;i++)
-        {
-        	int map[]=new int[26];
-        	int c;
-        	int cc=0;
-        	while((c=br.read())>96)
-        	{
-        		map[c-97]++;
-        		cc++;
-        	}
-        	while((c=br.read())!=-1&&c>96)
-        	{
-        		c=c-97;
-        		if(map[c]!=0)
-        		{
-        			map[c]--;
-        			cc--;
-        		}
-        	}
-        	if(cc==0)
-        		System.out.println("YES");
-        	else
-        		System.out.println("NO");
+    	Scanner sc=new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            if(K>N)
+            	K=K%N;
+            if(K==0)
+            {
+            	for(int j=0;j<N;j++)
+            	{
+            		int temp=sc.nextInt();
+            		System.out.print(temp+" ");
+            	}
+            	System.out.println();
+            }
+            else
+            {
+            	int arr[]=new int[N];
+            	for(int j=0;j<N;j++)
+            	{
+            		arr[j]=sc.nextInt();
+            	}
+            	for(int j=(N-K);j<N;j++)
+            		System.out.print(arr[j]+" ");
+            	for(int j=0;j<(N-K);j++)
+            		System.out.print(arr[j]+" ");
+            	System.out.println();
+            }
         }
-   }
+    }
 }
